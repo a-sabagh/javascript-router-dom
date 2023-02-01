11776537bc461598c7b7bda82286a939df5f8b64
a-sabagh <a.sabagh72@gmail.com>
2023-02-01 10:28:22 +0330

    event trigger handler using route and controller

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import style from './style.css'
+import {router} from './router.js'
+import {controller} from './controller.js'
+
+window.routeInit = (event) => {
+	event.preventDefault()
+	let target = event.target
+	let path = new URL(target.href).pathname
+	let page = router(path)
+	window.history.pushState({title: page.title},'',path)
+	window.document.title = page.title
+	controller(page)
+}
+
+window.onload = () => {
+	let href = window.location.href
+	let path = new URL(href).pathname
+	let page = router(path)
+	window.document.title = page.title
+	controller(page)
+}
