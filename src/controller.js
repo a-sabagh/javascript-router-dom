acd934ab37e38ac7194b4b7b4236941194b10c22
a-sabagh <a.sabagh72@gmail.com>
2023-02-01 10:28:03 +0330

    handle request path and update dom

--- src/controller.js
+++ src/controller.js
@@ -1,15 +1,7 @@
 function controller(page) {
 	let title = page.title
 	let content = page.content
-	document.getElementById("content").innerHTML = title + content
+	window.document.getElementById("content").innerHTML = content
 }
 
-function routeInit(event){
-	event.preventDefault()
-	let target = event.target
-	let path = target.href
-	let title = target.title
-	window.history.pushState({title: title},'',path)
-	window.document.title = title
-	let page = router(path)
-}
+export {controller}
